ece366ea22da08147ccd27eaef16f153bb8ab5a5
User <User@SIDRASALMAN>
2024-05-14 13:16:27 +0500

    Add project files.

---
+++ 5 time table for loop/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim answer As Integer
+        Dim table As Integer
+        table = 5
+        lbloutput.Text = " five times table" & vbCrLf
+
+        'Loop 10 times
+        For Counter = 1 To 10
+            ' calculate the answer to the curent sum 
+            answer = table * Counter
+            'appemd the answer onto the label with suitable formating 
+            lbloutput.Text = lbloutput.Text & table & " times " &
+           Counter & " = " & answer & vbCrLf
+        Next
+    End Sub
+End Class
---
+++ 5 time table for loop/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("5 time table for loop")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("5 time table for loop")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("614ec5a2-0a69-4fb5-8fcf-83cabbc3e02e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
